7579d67d7641cc2bb696427a3bddccfb55ec95ca
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-22 15:16:24 +0900

    hooks-01-starting-project/Section28-432

---
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setIngredients((prevState) => [...prevState, ingredient]);
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
