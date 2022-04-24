9edda30c9b3c2e9fa396660ae481531e9a5d4cca
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-25 08:38:37 +0900

    03-food_order/Section11-158

--- 04-optimization/src/App.js
+++ 04-optimization/src/App.js
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState();
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleHandler = () => {
-    setShowParagraph((prev) => !prev);
+  // ■useCallback
+  // dependencyにallowToggleが入っていないので、allowToggleがtrueに変わっても、古い値を参照し続けてしまう。
+  // const toggleHandler = useCallback(() => {
+  //   cconsole.log('allowToggle', allowToggle);
+  //   if (allowToggle) {
+  //     setShowParagraph((prev) => !prev);
+  //   }
+  // }, []);
+  const toggleHandler = useCallback(
+    () => {
+      console.log('allowToggle', allowToggle);
+      if (allowToggle) {
+        setShowParagraph((prev) => !prev);
+      }
+    },
+    // allowToggleをdependencyに入れているので、allowToggleが変わったらfunctionを作り直す。
+    [allowToggle]
+  );
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
 
-  // React.memp
+  // ■React.memp
   // ・DemoOutputはプリミティブのbooleanなので、memoが効く。
-  // ・ButtonはtoggleHandlerのfunctionなので、memoが効かない。
+  // ・ButtonはtoggleHandlerのfunctionなので、memoが効かない。⇒useCallbackを使えばmemoが効くようになる。
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={true} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleHandler}>Show</Button>
     </div>
   );
