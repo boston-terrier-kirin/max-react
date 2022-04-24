405b456d8a4a6eca714f5bf92250fd793ade11fb
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-25 06:05:56 +0900

    03-food_order/Section11-149

--- 03-food_order/src/components/layout/HeaderCartButton.js
+++ 03-food_order/src/components/layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numOfCartItems = cartCtx.items.reduce((acc, item) => {
+  const { items } = cartCtx;
+
+  const numOfCartItems = items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    setBtnIsHighlighted(true);
+
+    const timerId = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
