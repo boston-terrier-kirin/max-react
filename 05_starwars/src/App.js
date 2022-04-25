0cf57edb8b25687129233bdc01f313a8de33645e
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-25 11:46:35 +0900

    05_starwars/Section14-181

--- 05_starwars/src/App.js
+++ 05_starwars/src/App.js
@@ -1,26 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
+    setError(null);
 
-    const res = await fetch('https://swapi.dev/api/films');
-    const data = await res.json();
-    const movies = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        releaseDate: movie.release_date,
-        openingText: movie.opening_crawl,
-      };
-    });
+    try {
+      const res = await fetch('https://swapi.dev/api/films');
+      if (!res.ok) {
+        throw new Error('Something went wrong.');
+      }
 
-    setMovies(movies);
+      const data = await res.json();
+      const movies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        };
+      });
+
+      setMovies(movies);
+    } catch (err) {
+      setError(err.message);
+    }
+
     setIsLoading(false);
   };
 
@@ -31,6 +46,7 @@ function App() {
       </section>
       <section>
         {isLoading ? 'Loading...' : <MoviesList movies={movies} />}
+        {error && error}
       </section>
     </>
   );
