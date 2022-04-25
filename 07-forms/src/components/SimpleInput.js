a461f33f54ce51f59f3240c84842c8c47af52101
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-25 16:00:30 +0900

    07-forms/Section16-202

--- 07-forms/src/components/SimpleInput.js
+++ 07-forms/src/components/SimpleInput.js
@@ -1,9 +1,40 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+  const [name, setName] = useState('');
+  const [nameIsValid, setNameIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!name.trim()) {
+      setNameIsValid(false);
+      return;
+    }
+
+    setNameIsValid(true);
+
+    console.log('name: ', name);
+    console.log('nameRef.current.value: ', nameRef.current.value);
+
+    setName('');
+  };
+
+  const nameClasses = nameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className={nameClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          ref={nameRef}
+        />
+        {!nameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
