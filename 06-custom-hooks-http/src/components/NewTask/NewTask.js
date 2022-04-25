e6d435bcecc9b63131c9b8ea8d2497908173a01a
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-25 15:24:44 +0900

    15-custome-hook/Section15-196

--- 06-custom-hooks-http/src/components/NewTask/NewTask.js
+++ 06-custom-hooks-http/src/components/NewTask/NewTask.js
@@ -1,41 +1,25 @@
-import { useState } from 'react';
+import useHttp from '../../hooks/useHttp';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTask } = useHttp();
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-913ae-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
+    sendTask(
+      {
+        url: 'https://react-http-913ae-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText },
+      },
+      (data) => {
+        props.onAddTask({ id: data.name, text: taskText });
       }
-
-      const data = await response.json();
-
-      const generatedId = data.name;
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    );
   };
 
   return (
