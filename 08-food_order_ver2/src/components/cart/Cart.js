d8d5866bd9d3c2ca6959994364b112861af30682
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-26 06:10:15 +0900

    08-food_order_ver2/Section17-219

--- 08-food_order_ver2/src/components/cart/Cart.js
+++ 08-food_order_ver2/src/components/cart/Cart.js
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../ui/Modal';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
@@ -15,6 +17,14 @@ const Cart = (props) => {
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const cancelOrderHandler = () => {
+    setIsCheckout(false);
   };
 
   const cartItems = cartCtx.items.map((item) => (
@@ -28,6 +38,19 @@ const Cart = (props) => {
     />
   ));
 
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onHideCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onHideCart={props.onHideCart}>
       <ul className={classes['cart-items']}>{cartItems}</ul>
@@ -35,12 +58,7 @@ const Cart = (props) => {
         <span>Total Amount:</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout ? <Checkout onCancel={cancelOrderHandler} /> : modalActions}
     </Modal>
   );
 };
